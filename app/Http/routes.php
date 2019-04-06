d2cd77419d8c051816952b53aef83858fcbc2fd3
fadwa <fadwaana99@gmail.com>
2019-04-06 12:20:57 +0200

    attach & detach and sync

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,16 +1,86 @@
 <?php
+use App\User;
+use App\Role;
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
+///insert the role for some one user
+Route::get('/insert', function () {
+
+	$user = User::find(1);
+
+	$role = New Role();
+	$role->name = "admin";
+	$user->roles()->save($role);
+    
+});
+
+
+//read the role for some one user
+Route::get('/read', function () {
+
+	$user = User::find(1);
+
+	foreach ($user->roles as $role) {
+		
+		echo $role->name;
+	}
+    
+});
+
+
+//update the role for some one user
+Route::get('/update', function () {
+
+	$user = User::find(1);
+    
+   // $user->roles()->where('id','=',1)->update(['name'=>'fadwa']);
+
+    if($user->has('roles')){
+
+        //mean if have relationship called roles
+
+        foreach ($user->roles as $role) {
+
+        	if($role->name == "admin"){
+        		$role->name = "fadwa";
+        	    $role->save();
+        	}
+        	
+        }
+
+
+    }
+	
+    
+});
+
+/*Route::get('/delete',function(){ 
+	$user = User::find(1); 
+	$user->roles->where('id',4)->delete();
+});*/
+Route::get('/delete',function(){ 
+	$user = User::find(1); 
+	foreach ($user->roles as $role) {
+		
+		$role->where('id',4)->delete();
+	}
+});
+
+
+Route::get('/attach',function(){
+
+	$user = User::findOrFail(1);
+
+	/*
+	$user->roles()->attach(1);
+	that mean take user number one to role number 2
+	*/
+    /*
+    $user->roles()->detach(2);
+    that mean remove role number 2 for user number 1
+    */
+
+    $user->roles()->sync([3,4]);
+	
+
 });
